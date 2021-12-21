dfb627ba9703c6adf17bcab5b996fe19db375068
ibragimovamc <86709582+ibragimovamc@users.noreply.github.com>
2021-12-21 16:18:07 +1000

    some changes / added .gitignore

---
+++ save not storyboard/VCButton_to_main_page.swift
@@ -0,0 +1,29 @@
+//
+//  VCButton_to_main_page.swift
+//  first_idz
+//
+//  Created by Ибрагимова Татьяна on 08.12.2021.
+//
+
+import UIKit
+
+class VCButton_to_main_page: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
